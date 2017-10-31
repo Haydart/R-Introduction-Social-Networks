0753d5f92082e8d59cdb4aa476716ee249b307cc
Haydart <makowiecki.radoslaw@gmail.com>
2017-10-31 16:44:49 +0100

    Prepare graph

--- network_influence_spread_simulation.R
+++ network_influence_spread_simulation.R
@@ -2,5 +2,12 @@ library(igraph)
 
 raw_graph_data = read.csv(file="social_network_graph_data.csv", header=FALSE, sep=" ")
 
-graph_data = raw_graph_data[3:nrow(raw_graph_data), 1:2]
+graph_data <- raw_graph_data[3:nrow(raw_graph_data), 1:2]
+raw_graph <- graph_from_data_frame(graph_data)
+graph = simplify(raw_graph)
+
+# check if graph has 167 vertices & 5783 edges
+print(vcount(graph))
+print(ecount(graph))
+
 str(graph_data)
