d64ecc1f8027e39a48255c91f8df904a1d74008d
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-02 17:48:11 +0100

    Save plot as JPG image

--- network_influence_spread_simulation.R
+++ network_influence_spread_simulation.R
@@ -14,6 +14,11 @@ print(ecount(graph))
 
 # CONDUCT DISTRIBUTION SIMULATION
 
+iterations = 10
+legend_descriptions <- lapply(c(1:iterations), paste0, " gen")
+legend_descriptions
+iteration_colors = c("black", "blanchedalmond", "blue", "blueviolet", "cyan", "darkgoldenrod1", "darkolivegreen1", "firebrick1", "deeppink", "lightskyblue")
+
 # mark all nodes as non-infected
 for(vertex_index in V(graph)) {
   V(graph)[vertex_index]$infected = F
@@ -25,27 +30,22 @@ print(V(graph)$infected)
 random_index = sample(1:vcount(graph), 1)
 print(random_index)
 V(graph)[random_index]$infected = T
-V(graph)[random_index]$color = "black"
+V(graph)[random_index]$color = iteration_colors[1]
 
-# create 0-init matrix where the source-of-infection node is 1
+# create 0-init vertex size matrix where the source-of-infection node is 1
 size_matrix = matrix(0, nrow = vcount(graph), ncol = 1)
 size_matrix[random_index, 1] <- 1
-
-iterations = 10
-v <- rep("Gen", iterations)
-legend_descriptions <- c(1:10)
-legend_descriptions <- lapply(legend_descriptions, paste0, " gen")
-legend_descriptions
-iteration_colors = c("black", "blanchedalmond", "blue", "blueviolet", "cyan", "darkgoldenrod1", "darkolivegreen1", "firebrick1", "deeppink", "lightskyblue")
 
 #infect neighbors of sampled node
 for(neighbor in neighbors(graph, V(graph)[random_index])) {
   V(graph)[neighbor]$infected=T
-  V(graph)[neighbor]$color="red"
+  V(graph)[neighbor]$color=iteration_colors[2]
 }
 
+jpeg("plot.jpg", width = 1280, height = 768)
 plot(graph, rescale = TRUE, ylim=c(-1,1), xlim=c(-1.1,0.9), edge.arrow.size = .15, asp = 0, 
      vertex.label=NA, vertex.size=2.5+3*as.matrix(size_matrix), layout=layout.drl(graph))
 
-legend(-1.15, .75, y.intersp = .75, xjust = .5, yjust = .5, legend=as.vector(legend_descriptions), bty = "n", fill = iteration_colors, cex=0.8)
+legend(-1.15, .75, y.intersp = .75, xjust = .5, yjust = .5, legend=as.vector(legend_descriptions), bty = "n", fill = iteration_colors, cex=1.5)
+dev.off()
 
