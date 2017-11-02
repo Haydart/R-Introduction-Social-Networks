21f91ddbe0c7263c783335fe1687d6d0ea62e937
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-02 18:46:24 +0100

    Save plots from 5 randomly initialised attempts for 10 iterations each

--- network_influence_spread_simulation.R
+++ network_influence_spread_simulation.R
@@ -15,47 +15,46 @@ print(ecount(graph))
 
 # CONDUCT DISTRIBUTION SIMULATION
 
-iterations = 10
-legend_descriptions <- lapply(c(1:iterations), paste0, " gen")
+random_attempts_count <- 5
+iterations_within_attempt <- 10
+legend_descriptions <- lapply(c(1:iterations_within_attempt), paste0, " gen")
+plot_file_names <- lapply(c(1:random_attempts_count), paste0, "attempt.jpg")
 plot_colors <- c("black", "blanchedalmond", "blue", "blueviolet", "cyan", "darkgoldenrod1", "darkolivegreen1", "firebrick1", "deeppink", "lightskyblue")
-plot_file_names <- lapply(legend_descriptions, paste0, ".jpg")
 
-#plot_file_names <- lapply(plot_file_names, paste0, ".jpg")
-
-# mark all nodes as non-infected
-for(vertex_index in V(graph)) {
-  V(graph)[vertex_index]$infected = F
-  V(graph)[vertex_index]$color = "green"
-}
-print(V(graph)$infected)
-
-# set a random node to be infected
-random_index <- sample(1:vcount(graph), 1)
-print(random_index)
-V(graph)[random_index]$infected <- T
-V(graph)[random_index]$color <- plot_colors[1]
-
-# create 0-init vertex size matrix where the source-of-infection node is 1
-size_matrix = matrix(0, nrow = vcount(graph), ncol = 1)
-size_matrix[random_index, 1] <- 1
-
-# simulate 10 iterations
-for(index in 1:iterations) {
+for(attempt in 1:random_attempts_count) {
+  print(paste(as.character(attempt), "attempt"))
   
-  print(index)
-  
-  #infect neighbors of sampled node
-  for(neighbor in neighbors(graph, V(graph)[random_index])) {
-    V(graph)[neighbor]$infected <- T
-    V(graph)[neighbor]$color <- plot_colors[2]
+  # mark all nodes as non-infected
+  for(vertex_index in V(graph)) {
+    V(graph)[vertex_index]$infected = F
+    V(graph)[vertex_index]$color = "green"
   }
   
-  # plot the graph & save it to image
-  jpeg(as.character(plot_file_names[index]), width = 1280, height = 768)
+  # set a random node to be infected
+  random_index <- sample(1:vcount(graph), 1)
+  print(random_index)
+  V(graph)[random_index]$infected <- T
+  V(graph)[random_index]$color <- plot_colors[1]
+  
+  # create 0-init vertex size matrix where the source-of-infection node is 1
+  size_matrix = matrix(0, nrow = vcount(graph), ncol = 1)
+  size_matrix[random_index, 1] <- 1
+  
+  # simulate 10 iterations
+  for(index in 1:iterations_within_attempt) {
+    
+    #infect neighbors of sampled node
+    for(neighbor in neighbors(graph, V(graph)[random_index])) {
+      V(graph)[neighbor]$infected <- T
+      V(graph)[neighbor]$color <- plot_colors[2]
+    }
+  } 
+  
+  # plot the graph after 10 propagation iterations & save it to file
+  jpeg(as.character(plot_file_names[attempt]), width = 1280, height = 768)
   plot(graph, rescale = TRUE, ylim=c(-1,1), xlim=c(-1.1,0.9), edge.arrow.size = .15, asp = 0, 
        vertex.label=NA, vertex.size=2.5 + 1.5 * as.matrix(size_matrix), layout=layout.drl(graph))
   
   legend(-1.15, .75, y.intersp = .75, xjust = .5, yjust = .5, legend=as.vector(legend_descriptions), bty = "n", fill = plot_colors, cex=1.5)
   dev.off() 
 }
-
