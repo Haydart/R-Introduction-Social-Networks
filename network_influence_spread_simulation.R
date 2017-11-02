023d07f15d6dc2514f0a772c0f59360044a80245
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-02 17:32:40 +0100

    Add simulation iterations legend to plot

--- network_influence_spread_simulation.R
+++ network_influence_spread_simulation.R
@@ -25,11 +25,18 @@ print(V(graph)$infected)
 random_index = sample(1:vcount(graph), 1)
 print(random_index)
 V(graph)[random_index]$infected = T
-V(graph)[random_index]$color = "yellow"
+V(graph)[random_index]$color = "black"
 
 # create 0-init matrix where the source-of-infection node is 1
 size_matrix = matrix(0, nrow = vcount(graph), ncol = 1)
 size_matrix[random_index, 1] <- 1
+
+iterations = 10
+v <- rep("Gen", iterations)
+legend_descriptions <- c(1:10)
+legend_descriptions <- lapply(legend_descriptions, paste0, " gen")
+legend_descriptions
+iteration_colors = c("black", "blanchedalmond", "blue", "blueviolet", "cyan", "darkgoldenrod1", "darkolivegreen1", "firebrick1", "deeppink", "lightskyblue")
 
 #infect neighbors of sampled node
 for(neighbor in neighbors(graph, V(graph)[random_index])) {
@@ -37,7 +44,8 @@ for(neighbor in neighbors(graph, V(graph)[random_index])) {
   V(graph)[neighbor]$color="red"
 }
 
-plot(graph, rescale = TRUE, ylim=c(-.8,.8), xlim=c(-.8,.8), edge.arrow.size = .25, asp = 0, 
-     vertex.label=NA, vertex.size=2+3*as.matrix(size_matrix), layout=layout.drl(graph))
+plot(graph, rescale = TRUE, ylim=c(-1,1), xlim=c(-1.1,0.9), edge.arrow.size = .15, asp = 0, 
+     vertex.label=NA, vertex.size=2.5+3*as.matrix(size_matrix), layout=layout.drl(graph))
 
+legend(-1.15, .75, y.intersp = .75, xjust = .5, yjust = .5, legend=as.vector(legend_descriptions), fill = iteration_colors, cex=0.8)
 
