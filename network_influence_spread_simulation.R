eab31953bb08397fad8ec278eaee3b953b6a81fb
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-02 20:35:32 +0100

    Exclude already infected nodes from being infected again

--- network_influence_spread_simulation.R
+++ network_influence_spread_simulation.R
@@ -7,7 +7,8 @@ graph_data <- raw_graph_data[3:nrow(raw_graph_data), 1:2]
 str(graph_data)
 
 raw_graph <- graph_from_data_frame(graph_data, directed=TRUE)
-graph <- simplify(raw_graph)
+#graph <- simplify(raw_graph)
+graph <- erdos.renyi.game(p.or.m=.2, n=10, directed = TRUE)
 
 # check if graph has 167 vertices & 5783 edges
 print(vcount(graph))
@@ -15,8 +16,8 @@ print(ecount(graph))
 
 # CONDUCT DISTRIBUTION SIMULATION
 
-random_attempts_count <- 5
-iterations_within_attempt <- 10
+random_attempts_count <- 1
+iterations_within_attempt <- 2
 legend_descriptions <- lapply(c(1:iterations_within_attempt), paste0, " gen")
 plot_file_names <- lapply(c(1:random_attempts_count), paste0, "attempt.jpg")
 plot_colors <- c("black", "blanchedalmond", "blue", "blueviolet", "cyan", "darkgoldenrod1", "darkolivegreen1", "firebrick1", "deeppink", "lightskyblue")
@@ -40,20 +41,34 @@ for(attempt in 1:random_attempts_count) {
   size_matrix = matrix(0, nrow = vcount(graph), ncol = 1)
   size_matrix[random_index, 1] <- 1
   
-  # simulate 10 iterations
+  infection_sources <- V(graph)[random_index]
+  
+  # simulate 10 iterations of infection propagation
   for(index in 1:iterations_within_attempt) {
     
+    neighbors_to_be_infected <- list()
+    
+    for(infection_source in infection_sources) {
+      print(paste("infection source", infection_source))
+      neighbors_to_be_infected <- union(neighbors_to_be_infected, neighbors(graph, V(graph)[infection_source], mode = "out"))
+    }
+    
+    neighbors_to_be_infected <- setdiff(neighbors_to_be_infected, V(graph)[infected == T])
+    
+    infection_sources <- neighbors_to_be_infected
+    
     #infect neighbors of sampled node
-    for(neighbor in neighbors(graph, V(graph)[random_index])) {
+    for(neighbor in neighbors_to_be_infected) {
       V(graph)[neighbor]$infected <- T
-      V(graph)[neighbor]$color <- plot_colors[2]
+      V(graph)[neighbor]$color <- plot_colors[index+1]
     }
   } 
   
   # plot the graph after 10 propagation iterations & save it to file
   jpeg(as.character(plot_file_names[attempt]), width = 1280, height = 768)
-  plot(graph, rescale = TRUE, ylim=c(-1,1), xlim=c(-1.1,0.9), edge.arrow.size = .15, asp = 0, 
-       vertex.label=NA, vertex.size=2.5 + 1.5 * as.matrix(size_matrix), layout=layout.drl(graph), main = paste("Random attempt no. ", attempt))
+  #plot(graph, rescale = TRUE, ylim=c(-1, 1), xlim=c(-1.1, 0.9), edge.arrow.size = .5, asp = 0, 
+   #    vertex.label=NA, vertex.size=2.5 + 1.5 * as.matrix(size_matrix), layout=layout.drl(graph), main = paste("Random attempt no. ", attempt))
+  plot(graph)
   
   legend(-1.15, .75, y.intersp = .75, xjust = .5, yjust = .5, legend=as.vector(legend_descriptions), bty = "n", fill = plot_colors, cex=1.5)
   dev.off() 
