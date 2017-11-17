56bbe2c2e5698111dbeaa4ce1daa468ee715d0a7
Haydart <makowiecki.radoslaw@gmail.com>
2017-11-17 09:26:57 +0100

    Print code quality data summary

--- code_quality_prediction.R
+++ code_quality_prediction.R
@@ -2,3 +2,5 @@ trainOrig = read.csv(file="lucene2.2train.csv", header=TRUE, sep=",")
 testOrig = read.csv(file="lucene2.4test.csv", header=TRUE, sep=",")
 
 print(head(trainOrig, n=2))
+
+print(mlr::summarizeColumns(trainOrig))
